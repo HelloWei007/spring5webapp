fce3c0833e2999f11b4b1a2c1b8f711c7ce3c5a7
HelloWei007 <jiawei.tang1996@gmail.com>
2018-09-08 17:13:45 +0200

    spring boot ini project

---
+++ src/main/java/jiawei/project/framework/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,12 @@
+package jiawei.project.framework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+}
